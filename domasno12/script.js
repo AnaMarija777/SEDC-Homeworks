4f9e3fdf6777ab050668b674a479ac6e31640bef
AnaMarija777 <152323627+AnaMarija777@users.noreply.github.com>
2023-12-15 23:20:54 +0100

    Add files via upload

---
+++ domasno12/script.js
@@ -0,0 +1,10 @@
+// function tellStory
+let name= ["Anthony Owe"];
+let mood= ["joyful"];
+let activity=["driving"];
+
+let sentence = `This is ${name}. ${name} is a nice person.Today they are ${mood}.They are ${activity} all day.The end. `;
+alert(sentence);
+
+
+
