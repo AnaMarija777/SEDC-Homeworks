3874657150731c805330ab0fad340ee92c0cc989
AnaMarija777 <152323627+AnaMarija777@users.noreply.github.com>
2023-12-19 17:18:31 +0100

    Add files via upload

---
+++ domasno13/script.js
@@ -0,0 +1,10 @@
+console.log('Dog Years');
+let Years = parseInt(prompt('Vnesete godini'))
+function dogYears(godini){
+    return godini * 7;
+}
+console.log(dogYears(Years));
+function humanYears(coveckiGodini){
+    return coveckiGodini/7
+}
+console.log(humanYears(Years));
